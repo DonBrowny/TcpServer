2e588938205b9505bd4f5dcc9e3448cd2f788f8e
HellCat <kishore13ask@gmail.com>
2017-07-22 11:49:44 +0530

    changed

--- Server/server.py
+++ Server/server.py
@@ -6,6 +6,7 @@ class BaseServer(TCPServer):
     def handle_stream(self, stream, address):
         self._stream = stream
         self._address = address
+        print(address)
         self.read_request()
 
     def read_request(self):
@@ -13,9 +14,12 @@ class BaseServer(TCPServer):
 
     def handle_request(self,data):
         requestBody = data[:-1]
+        print(requestBody)
 
 if __name__ == '__main__':
     server = BaseServer()
-    server.bind(8081,"192.168.0.2")
-    server.start(0)
+    #server.bind(8081,"192.168.0.2")
+    server.bind(8081)
+
+    server.start(1)
     IOLoop.current().start()
